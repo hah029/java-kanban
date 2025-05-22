82ecc603a6fb90b0ec9c301e5459c06b8b02a562
Roman <roma.chekalin@gmail.com>
2025-05-22 12:41:40 +0300

    Fix - Remove unused imports

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-import management.Managers;
-import management.TaskManager;
-import task.Epic;
-import task.Subtask;
-import task.Task;
-
 public class Main {
     public static void main(String[] args) {}
 }
